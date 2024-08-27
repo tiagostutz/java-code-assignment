dae144715c211084c2dcb29767507b46c1727ac6
Tiago Stutz <tiago.de.oliveira.stutz@ingka.ikea.com>
2024-08-27 13:49:55 +0200

    feat: add max capacity to location

--- src/main/java/com/fulfilment/application/monolith/location/LocationGateway.java
+++ src/main/java/com/fulfilment/application/monolith/location/LocationGateway.java
@@ -10,14 +10,14 @@ public class LocationGateway implements LocationResolver {
   private static final List<Location> locations = new ArrayList<>();
 
   static {
-    locations.add(new Location("ZWOLLE-001", 1));
-    locations.add(new Location("ZWOLLE-002", 2));
-    locations.add(new Location("AMSTERDAM-001", 5));
-    locations.add(new Location("AMSTERDAM-002", 3));
-    locations.add(new Location("TILBURG-001", 1));
-    locations.add(new Location("HELMOND-001", 1));
-    locations.add(new Location("EINDHOVEN-001", 2));
-    locations.add(new Location("VETSBY-001", 1));
+    locations.add(new Location("ZWOLLE-001", 1, 40));
+    locations.add(new Location("ZWOLLE-002", 2, 50));
+    locations.add(new Location("AMSTERDAM-001", 5, 100));
+    locations.add(new Location("AMSTERDAM-002", 3, 75));
+    locations.add(new Location("TILBURG-001", 1, 40));
+    locations.add(new Location("HELMOND-001", 1, 45));
+    locations.add(new Location("EINDHOVEN-001", 2, 70));
+    locations.add(new Location("VETSBY-001", 1, 90));
   }
 
   @Override
